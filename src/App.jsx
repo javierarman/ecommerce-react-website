cd9ca5560ed653400cb843eace2712a715ca1a4c
JavierArmando <croping20@gmail.com>
2026-06-05 14:49:01 -0500

    Added auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Home from './pages/Home';
 import Auth from './pages/Auth';
 import Checkout from './pages/Checkout';
 import Navbar from './components/Navbar';
+import AuthProvider, { AuthContext } from './context/AuthContext';
 
 
 function App() {
   
-
   return (
+    <AuthProvider>
     <div className='app'>
       <Navbar />
       <Routes>
@@ -18,6 +19,7 @@ function App() {
         <Route path="/checkout" element={<Checkout />}/>
       </Routes>
     </div>
+    </AuthProvider>
   )
 }
 
